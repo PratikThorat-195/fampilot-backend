2f64cd2145b12c48849b7ce1557cffe725f4905c
PratikThorat-195 <pratikthorat195@gmail.com>
2026-09-10 16:12:04 +0530

    Project Setup

---
+++ src/main/java/fampilot_backend/FampilotBackendApplication.java
@@ -0,0 +1,13 @@
+package fampilot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FampilotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FampilotBackendApplication.class, args);
+	}
+
+}
